fb8a31e41c821aa9496edffec7d7264e21dd4072
chen-tianhao <fm0517@hotmail.com>
2024-05-07 10:58:18 +0800

    runable version for real type parameters

---
+++ SerializableAction.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class SerializableAction : ISerializable
+{
+    private readonly byte[] _targetData;
+    private readonly string _methodName;
+
+    public SerializableAction(Delegate action)
+    {
+        _methodName = action.Method.Name;
+        _targetData = SerializeTarget(action.Target);
+    }
+
+    protected SerializableAction(SerializationInfo info, StreamingContext context)
+    {
+        if (info == null)
+            throw new ArgumentNullException(nameof(info));
+
+        _methodName = info.GetString("MethodName");
+        _targetData = (byte[])info.GetValue("TargetData", typeof(byte[]));
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        if (info == null)
+            throw new ArgumentNullException(nameof(info));
+
+        info.AddValue("MethodName", _methodName);
+        info.AddValue("TargetData", _targetData);
+    }
+
+    public void Invoke(params object[] args)
+    {
+        object target = DeserializeTarget(_targetData);
+        MethodInfo method = target.GetType().GetMethod(_methodName);
+        // MethodInfo genericMethod = method.MakeGenericMethod(args.Select(a => a.GetType()).ToArray());
+        // genericMethod.Invoke(target, args);
+        method.Invoke(target, args);
+    }
+
+    private static byte[] SerializeTarget(object target)
+    {
+        if (target == null)
+            return null;
+
+        using (MemoryStream stream = new MemoryStream())
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, target);
+            return stream.ToArray();
+        }
+    }
+
+    private static object DeserializeTarget(byte[] data)
+    {
+        if (data == null)
+            return null;
+
+        using (MemoryStream stream = new MemoryStream(data))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(stream);
+        }
+    }
+}
---
+++ TestCase.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+[Serializable]
+public class MyClass
+{
+    // public SerializableAction MyAction { get; set; }
+    public SerializableAction MyAction1 { get; set; }
+    public SerializableAction MyAction2 { get; set; }
+    public SerializableAction MyAction3 { get; set; }
+    public string Desc { get; set; } = "This is the test case for serialization";
+    public void MyMethod<T>(T message)
+    {
+        Console.WriteLine(message);
+    }
+    public void MyFunc1(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public void MyFunc2()
+    {
+        Console.WriteLine("This is MyFunc2.");
+    }
+    public void MyFunc3(int i, string message)
+    {
+        Console.WriteLine($"[{i}] : {message}");
+    }
+}
+
+class TestCase
+{
+    static void Main()
+    {
+        var obj = new MyClass();
+        // obj.MyAction = new SerializableAction(new Action<string>(obj.MyMethod<string>));
+        obj.MyAction1 = new SerializableAction(new Action<string>(obj.MyFunc1));
+        obj.MyAction2 = new SerializableAction(new Action(obj.MyFunc2));
+        obj.MyAction3 = new SerializableAction(new Action<int, string>(obj.MyFunc3));
+
+        var serialized = JsonConvert.SerializeObject(obj);
+        Console.WriteLine("Serialized:");
+        Console.WriteLine(serialized);
+
+        var deserialized = JsonConvert.DeserializeObject<MyClass>(serialized);
+        // deserialized.MyAction.Invoke("Hello, Method");
+        deserialized.MyAction1.Invoke("Hello, Func");
+        deserialized.MyAction2.Invoke();
+        deserialized.MyAction3.Invoke(999, "AAA");
+    }
+}
